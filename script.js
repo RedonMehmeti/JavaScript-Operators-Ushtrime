9c8a8c32d2299ab7a8b4f989f931d9590b2708f6
RedonMehmeti <redonmehmeti2019@gmail.com>
2024-06-08 10:22:15 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// let x = 5;
+// let y = 5;
+
+// let z = x + y;
+
+// document.getElementById("demo").innerHTML = " The sum of x + y is "  + z;
+
+
+// let x = 5;
+// let y = 2;
+// let z = x + y;
+// document.getElementById("demo").innerHTML = z;
+
+
+let a = 3;
+let x = (100 + 50) * a;
+document.getElementById("demo").innerHTML = x;
